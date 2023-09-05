30b0344ad659f87c9683606e144e53a7b81cb3b4
Daldek <piotr.de.bever@gmail.com>
2023-09-05 14:47:37 +0200

    Error handling

--- imgw.py
+++ imgw.py
@@ -4,27 +4,62 @@ public_data_url = 'https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjn
 
 # these should be user's input. Currently fixed values for testing purpose
 data_type = 'dane_hydrologiczne'  # unchangeable for now
-data_period_type = input('Choose: "dobowe", "miesieczne" or "polroczne_i_roczne": ')
-year = input('Range of years from 1951: ')
-if data_period_type == 'dobowe':
-    month = int(input('Numerical values from 1 to 12: '))  # range 1-12
-if data_period_type == 'polroczne_i_roczne':
-    data_variable = input('Choose: "T", "Q" or "H": ')
 
-# join variables to address
-if data_period_type == 'dobowe':
-    if month < 10:
-        month = f'0{month}'
-    url = f'{public_data_url}/{data_type}/{data_period_type}/{year}/codz_{year}_{month}.zip'
-    f = f'codz_{year}_{month}.zip'
-elif data_period_type == 'miesieczne':
-    url = f'{public_data_url}/{data_type}/{data_period_type}/{year}/mies_{year}.zip'
-    f = f'mies_{year}.zip'
-else:
-    url = f'{public_data_url}/{data_type}/{data_period_type}/{year}/polr_{data_variable}_{year}.zip'
-    f = f'polr_{data_variable}_{year}.zip'
+while True:
+    # First input
+    data_period_type = input('Choose: "dobowe", "miesieczne" or "polroczne_i_roczne": ')
+    if data_period_type not in ['dobowe', 'miesieczne', 'polroczne_i_roczne']:
+        print('Wrong input')
+        break
+    
+    # Second input
+    year = input('Range of years from 1951 to 2022: ')
+    try:
+        year = int(year)
+    except ValueError:
+        print('The value given is not an integer')
+        break
+    else:
+        year = int(year)
+        if year < 1951 or year > 2022:
+            print("Year given out of data range")
+            break
+    
+    # Third input
+    if data_period_type == 'dobowe':
+        month = input('Numerical values from 1 to 12: ')  # range 1-12
+        try:
+            month = int(month)
+        except ValueError:
+            print('The value given is not an integer')
+            break
+        else:
+            if month < 1 or month > 12:
+                print("Wrong input")
+                break
+    
+    # Fourth input
+    if data_period_type == 'polroczne_i_roczne':
+        data_variable = input('Choose: "T", "Q" or "H": ')
+        if data_variable not in ['T', 'Q', 'H']:
+            print('Wrong input')
+            break
 
-print(url)
+    # join variables to address
+    if data_period_type == 'dobowe':
+        if month < 10:
+            month = f'0{month}'
+        url = f'{public_data_url}/{data_type}/{data_period_type}/{year}/codz_{year}_{month}.zip'
+        f = f'codz_{year}_{month}.zip'
+    elif data_period_type == 'miesieczne':
+        url = f'{public_data_url}/{data_type}/{data_period_type}/{year}/mies_{year}.zip'
+        f = f'mies_{year}.zip'
+    else:
+        url = f'{public_data_url}/{data_type}/{data_period_type}/{year}/polr_{data_variable}_{year}.zip'
+        f = f'polr_{data_variable}_{year}.zip'
 
-# get data from IMGW
-wget.download(url, f)
+    print(url)
+
+    # get data from IMGW
+    wget.download(url, f)
+    break
