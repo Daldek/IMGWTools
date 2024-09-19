799e540543dbe3939a974c7a023b66461c67100d
Daldek <38719982+Daldek@users.noreply.github.com>
2024-09-19 10:45:53 +0200

    adapt code to object-oriented programming standards

--- imgw_api.py
+++ imgw_api.py
@@ -1,42 +1,91 @@
 import requests
 import json
-from statistics import mean
 
 
-def establish_connection(data_type):
-    url = 'https://danepubliczne.imgw.pl/api/data/' + data_type
-    
-    # establish connection with IMGW
-    r = requests.get(url)
-    print("Status code: ", r.status_code)
-    return r
+class IMGWAPI:
+    """
+    A class to interact with the IMGW API to fetch weather and hydrological data.
+    """
 
-def get_data(data_type):
-    r = establish_connection(data_type)
+    def __init__(self):
+        """
+        Initializes the IMGWAPI instance with the base URL.
+        """
+        self.base_url = "https://danepubliczne.imgw.pl/api/data/"
 
-    # data to json
-    api_data = r.json()
+    def establish_connection(self, url):
+        """
+        Establishes a connection to the IMGW API and retrieves data.
 
-    # get dict keys from the first item
-    api_keys = api_data[0].keys()
-    print(api_keys)
-    return api_data
+        Args:
+            url (str): The URL to fetch data from.
+
+        Returns:
+            Response object: The response from the API.
+        """
+        r = requests.get(url)
+        print("Status code:", r.status_code)
+        return r
+
+    def get_data(self, url):
+        """
+        Fetches data from the IMGW API and converts it to JSON.
+
+        Args:
+            url (str): The URL to fetch data from.
+
+        Returns:
+            list: A list of dictionaries containing the data from the API.
+        """
+        r = self.establish_connection(url)
+        api_data = r.json()
+        return api_data
+
+    @staticmethod
+    def save_json_to_file(data):
+        """
+        Saves the fetched data to a JSON file.
+
+        Args:
+            data (list): The data to save.
+
+        Returns:
+            int: Returns 1 upon successful save.
+        """
+        with open(
+            r"./data/downloaded/imgw_api_response.json", "w", encoding="utf-8"
+        ) as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+        return 1
 
 
-def save_json_to_file(r):
-    with open('imgw_data.json', 'w', encoding='utf-8') as f:
-        json.dump(r, f, ensure_ascii=False, indent=4)
-    return 1
+class HYDRO(IMGWAPI):
+    """
+    A class to interact with the IMGW API to fetch hydrological data.
+    """
 
+    def __init__(self, station_id=None, data_format="json"):
+        """
+        Initializes the HYDRO instance with the specified parameters.
 
-def calc_average(r, variable):
-    vars = []
-    for x in r:
-        vars.append(float(x[variable]))
-    average = mean(vars)
-    return average
+        Args:
+            station_id (str): ID of the station.
+            station_name (str): Name of the station.
+            data_format (str): Format of the data (default is 'json').
+        """
+        super().__init__()
+        self.url = f"{self.base_url}hydro2"
+        self.station_id = station_id
+        self.data_format = data_format
+        self.data = None
 
-data_type = input('Hydro or synop?: ')
-response = get_data(data_type)
-# var = input('Variable: ')
-# print(calc_average(response, var))
+    def get_hydro_data(self):
+        """
+        Fetches hydrological data from the IMGW API.
+
+        Returns:
+            list: A list of dictionaries containing the hydrological data from the API.
+        """
+        self.url += f"/id/{self.station_id}"
+        self.data = self.get_data(self.url)
+        return self.data
